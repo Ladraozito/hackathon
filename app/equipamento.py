e60eaec66f1d2feb370a393c6cc8fecaa2b3a600
hugo.castor15@gmail.com <hugo.castor15@gmail.com>
2019-10-24 11:40:44 -0300

    modelando a pagina EQUIPAMENTO

--- app/equipamento.py
+++ app/equipamento.py
@@ -2,10 +2,12 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+
 from main import LoginApp
-class MainApp(App):
-    def build(self):
+class EquipamentoApp(Screen):
+    def build(self):    
         return Builder.load_file('equipamento.kv')
+            
 
 
-MainApp().run()
+
--- app/switch.py
+++ app/switch.py
@@ -4,7 +4,7 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from equipamento import EquipamentoApp
 from main import LoginApp
 from home2 import MeuMenuApp
 
@@ -12,6 +12,7 @@ Builder.load_file('kv_modules/main_widget.kv')
 sm = ScreenManager()
 sm.add_widget(LoginApp(name='login'))
 sm.add_widget(MeuMenuApp(name='menu'))
+sm.add_widget(EquipamentoApp(name='equipamento'))
 
 class MainApp(App):
     def build(self):
@@ -32,6 +33,12 @@ class MainApp(App):
         self.nome = dados[0].split(',')[1]
         self.fazenda = dados[1].split(',')[1]
         arquivo.close()
+    def telaEquipamento(self):
+        sm.current = 'equipamento'
+
+
+   
+
 
 
 if __name__ == "__main__":
