243d9833021ab152b551106949ca9bba2401ef0c
ladraozito <gabrielbitercourt@gmail.com>
2019-10-23 23:37:12 -0400

    mudanças corrigidas

--- app/Main.py
+++ app/Main.py
@@ -3,39 +3,59 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix import button
 
-
 main_widget_kv = """
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 
-    FloatLayout:
-        canvas.before:
-            Color:
-                rgba: get_color_from_hex('#CDFFCC')
-            Rectangle:
-                pos: self.pos
-                size: self.size
-        Image:
-            size_hint: .3, .1,
-            pos_hint: {"center_x": .5, "center_y": .75}
-            st_x: .3
-                text_size: self.size
-                halign: 'left'
-                valign: 'middle'
-                color: get_color_from_hex('#000000')    
-                Label:
-            TextInput:
-                id : fazenda
-                multiline: False
-            Label:
-            Button:
-                size_hint: 2., 2.
-                text: "REGISTRAR-SE"
-                on_press :
-                    butao = True
-                on_release: app.login()
-                
-    """
+FloatLayout:
+    canvas.before:
+        Color:
+            rgba: get_color_from_hex('#CDFFCC')
+        Rectangle:
+            pos: self.pos
+            size: self.size
+    Image:
+        size_hint: .3, .1,
+        pos_hint: {"center_x": .5, "center_y": .75}
+        source: 'logotamba.png'
+    Label:
+        id: msg
+        size_hint: .7, .1
+        pos_hint: {"center_x": .5, "center_y": .55}
+        text:
+    GridLayout:
+        cols: 1
+        spacing: 5
+        size_hint: .7, .37
+        pos_hint: {"center_x": .5, "center_y": .5}
 
+        Label:
+            text: "Nome do proprietario:"
+            size_hint_x: .3
+            text_size: self.size
+            halign: 'left'
+            valign: 'middle'
+            color: get_color_from_hex('#000000')
+        TextInput:
+            id: nome
+            multiline: False
+        Label:
+            text: "Nome da fazenda:"
+            size_hint_x: .3
+            text_size: self.size
+            halign: 'left'
+            valign: 'middle'
+            color: get_color_from_hex('#000000')    
+            Label:
+        TextInput:
+            id: fazenda
+            multiline: False
+        
+        Label:
+        Button:
+            size_hint: 2., 2.
+            text: "REGISTRAR-SE"
+            on_release: app.login(nome.text, fazenda.text)
+"""
 
 
 class LoginApp(App):
