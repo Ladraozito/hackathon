6efd16e316e3fc2657eb975cf514f0b61c04bda2
ladraozito <gabrielbitercourt@gmail.com>
2019-10-24 07:23:55 -0400

    importações desnecessárias removidas

--- app/home2.py
+++ app/home2.py
@@ -1,8 +1,3 @@
-import kivy 
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
   
 
