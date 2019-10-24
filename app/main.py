de34cf52526d81ac0dd164ba2664baa3cd4a4962
ladraozito <gabrielbitercourt@gmail.com>
2019-10-24 03:22:33 -0400

    arquivos reorganizados

--- app/main.py
+++ app/main.py
@@ -5,13 +5,7 @@ from kivy.uix import button
 from kivy.uix.screenmanager import Screen
 
 
-class LoginApp(App):
-    def build(self):
-        return Builder.load_file('kv_modules/main_widget.kv')
-
+class LoginApp(Screen):
     def login(self, nome, fazenda):
         self.nome = nome
         self.fazenda = fazenda
-
-if __name__ == "__main__":
-    LoginApp().run()
--- app/switch.py
+++ app/switch.py
@@ -1,3 +1,16 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+from main import LoginApp
+
+Builder.load_file('kv_modules/main_widget.kv')
+sm = ScreenManager()
+sm.add_widget(LoginApp(name='login'))
+
+class MainApp(App):
+    def build(self):
+        return sm
+
+if __name__ == "__main__":
+    MainApp().run()
