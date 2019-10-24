baa7ea598d96570da6e86b0a2579410b07c66774
ladraozito <gabrielbitercourt@gmail.com>
2019-10-24 07:05:59 -0400

    Transição de telas aplicado

--- app/home2.py
+++ app/home2.py
@@ -3,17 +3,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
   
-arquivo = open('dados.txt','r',encoding="utf-8")
-dados = arquivo.readlines()
-nome = dados[0].split(',')[1]
-fazenda = dados[1].split(',')[1]
-print(nome,fazenda)
-arquivo.close()
 
-class MeuMenuApp(App):
-    def build(self):
-        return Builder.load_file('home2.kv')
-
-
-MeuMenuApp().run()  
+class MeuMenuApp(Screen):
+    pass
