4e2d129973a2cbb3bd463c3d0e749be8d6bd2f76
ladraozito <gabrielbitercourt@gmail.com>
2019-10-24 23:35:28 -0400

    tentativa de correção

--- app/main.py
+++ app/main.py
@@ -10,37 +10,6 @@ from login import LoginApp
 
 import os.path
 import sqlite3 as lite
-from datetime import date 
-class MainApp(App):
-
-    def __init__(self):
-        App.__init__(self)
-        self.sm = ScreenManager()
-        self.sm.add_widget(LoginApp(name='login'))
-        self.sm.add_widget(MeuMenuApp(name='menu'))
-        self.sm.add_widget(EquipamentoApp(name='equipamento'))
-        if not os.path.exists('./dados.db'):
-            self.conn = lite.connect('./dados.db')
-            cursor = self.conn.cursor()
-            cursor.execute('''CREATE TABLE cadastroInicio(
-                nome varchar(255) NOT NULL,
-                fazenda varchar(255) NOT NULL
-                )''')
-            cursor.execute('''CREATE TABLE equipamento(
-                desc_item varchar(255) NOT NULL,
-                valor_item integer NOT NULL,
-                tempoUso integer  NOT NULL,
-                vidaUtil integer NOT NULL
-            )''')
-        
-        self.conn = lite.connect('./dados.db')
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM cadastroInicio")
-        dados = cursor.fetchall()
-        if len(dados) > 0: 
-            self.sm.current = 'menu'
-        else:
-            self.sm.current = 'login'
 from datetime import date
 
 Builder.load_file('kv_modules/widgets.kv')
@@ -69,17 +38,6 @@ class MainApp(App):
     def build(self):
         return sm
 
-    def build(self):
-        return self.sm
-    def printlog(self, message):
-        with open('./log.txt','a') as f: f.write(message+"\n")
-    def salvaLogin(self,pnome,pfazenda):
-        self.printlog(pnome.text)
-        self.printlog(pfazenda.text)
-        cursor = self.conn.cursor()
-        cursor.execute('INSERT INTO cadastroInicio (nome, fazenda) VALUES (?, ?)',(pnome.text, pfazenda.text))
-        self.conn.commit()
-        self.sm.current = 'menu'
     def login(self, nome, fazenda):
         self.nome = nome
         self.fazenda = fazenda
@@ -87,25 +45,28 @@ class MainApp(App):
             string = f'nome,{nome}\n'
             string += f'fazenda,{fazenda}\n'
             arquivo.write(string)
-        self.sm.current = 'menu'
-    
+        sm.current = 'menu'
+
     def verificar(self):
-        arquivo = open('dados.txt','r',encoding="utf-8")
+        arquivo = open('dados.txt', 'r', encoding="utf-8")
         dados = arquivo.readlines()
         self.nome = dados[0].split(',')[1]
         self.fazenda = dados[1].split(',')[1]
         arquivo.close()
 
     def telaEquipamento(self):
-        self.sm.current = 'equipamento'
-     
-   def guardaDados(self,descricaoitem,valoritem,tempoUsoitem,vidaUtilItem):
+        sm.current = 'equipamento'
+
+    def telaDespesas(self):
+        sm.current = 'despesas'
+
+    def guardaDados(self, descricaoitem, valoritem, tempoUsoitem, vidaUtilItem):
         if not valoritem:
             valoritem = 0.0
         else:
             valoritem = valoritem.replace(",", ".")
             valoritem = float(valoritem)
-        
+
         if not vidaUtilItem:
             vidaUtilItem = 0.0
         else:
@@ -115,19 +76,20 @@ class MainApp(App):
             tempoUsoitem = 0.0
         else:
             tempoUsoitem = float(tempoUsoitem)
-        
+
         if not vidaUtilItem:
             perdaAnual = 0
         else:
-            perdaAnual = valoritem/vidaUtilItem
-        
-        self.valoratualItem = (tempoUsoitem*perdaAnual-valoritem)
-        self.cursor.execute('''INSERT INTO equipamento (desc_item,valor_item,tempoUso,vidaUtil)
-            VALUES(?, ?, ?, ?)''',(descricaoitem,valoritem,tempoUsoitem,vidaUtilItem))               
+            perdaAnual = valoritem / vidaUtilItem
+
+        self.valoratualItem = (tempoUsoitem * perdaAnual - valoritem)
+        cursor.execute('''INSERT INTO equipamento (desc_item,valor_item,tempoUso,vidaUtil)
+            VALUES(?, ?, ?, ?)''', (descricaoitem, valoritem, tempoUsoitem, vidaUtilItem))
+
     def voltamenu(self):
-        self.sm.current = 'menu'
+        sm.current = 'menu'
 
 
 if __name__ == "__main__":
-    Builder.load_file('kv_modules/widgets.kv')
+    sm.current = 'login'
     MainApp().run()
