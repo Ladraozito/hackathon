85d7989f8638342d3d552545d04c1738f3b22a07
ladraozito <gabrielbitercourt@gmail.com>
2019-10-24 22:36:00 -0400

    Revert "seila"
    This reverts commit 55753fb995d8a5fa554cd505542b37a85b115be1.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 from equipamento import EquipamentoApp
