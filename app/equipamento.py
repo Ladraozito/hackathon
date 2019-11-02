363c863b5b1c73e077fda210daa083603a4d26e0
Ladraozito <gabrielbitercourt@gmail.com>
2019-11-02 15:34:46 -0400

    ..

--- app/equipamento.py
+++
@@ -1,9 +0,0 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-
-class EquipamentoApp(Screen):
-    def limpar(self):
-        self.ids['descricao'].text = ''
-        self.ids['valor'].text = ''
-        self.ids['TempoUso'].text = ''
-        self.ids['vidaUtil'].text = ''
--- app/relatorioGERAL.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-
-class RelatorioApp(Screen):
-    def relatorioGERAL(self):
-       # self.ids['gastosTOTAIS'].text = ''
-        self.ids['texto'].text = ''
-        self.ids['gastoMes'].text = ''
